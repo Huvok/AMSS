f5c6f52a998119ede96ff922019394416b22ab83
Daniel Hernandez <tamdannyhdz@gmail.com>
2018-11-15 17:00:19 -0600

    Add webapp directory and initial changes

---
+++ webapp/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import './index.css';
+import Login from './components/Login';
+import * as serviceWorker from './serviceWorker';
+
+const theme = {
+  black: '#47525e',
+  lightgreen: '#aaf3cb',
+  green: '#14ce66',
+  darkgreen: '#158938'
+}
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <Login/>
+  </ThemeProvider>,
+document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
